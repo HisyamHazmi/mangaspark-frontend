0dcaedbdd73d5ce3865ed54dfcc0ac5c5955b571
hisyamhazmi <muhamad.noor.hisyam07@gmail.com>
2025-08-16 00:37:17 +0800

    Refactor: Remove unnecessary semicolons in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mangaspark-frontend/'
-});
+  base: '/mangaspark-frontend/'  // <-- This is VERY important for GitHub Pages
+})
